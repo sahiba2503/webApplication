409a2ad8ac30437b40618f4964c505adca128817
sahiba <25sahiba03@gmail.com>
2026-08-22 23:29:02 +0530

    add http get and post method

--- server/Backend.js
+++ server/Backend.js
@@ -6,6 +6,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let todos=[
+    {
+        name:"Task name",
+        id:1,
+        description:"example"
+    }
+]
+
+app.get("/gettodo",(req,res)=>{
+      res.json(todos);
+});
+
+app.post("/posttodo",(req,res)=>{
+    let task={
+        name:req.body.name,
+        id:todos.length + 1,
+        description:req.body.description
+            }
+            todos.push(task);
+            res.json(todos);
+});
+
+
+
 app.listen(3000,()=>{
     console.log("server is running on 3000");
 })
--- userInterface/frontend/Task.jsx
+++ userInterface/frontend/Task.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react"
 
 
 function Task() {
+  const[todos,setTodos] = useState([]);
+  const [todoName,setTodoName] = useState([]);
+  const [todoDes,setTodoDes] = useState([]);
+ 
+  
+    useEffect(()=>{
+ fetch("http://localhost:3000/gettodo")
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log(data);
+      setTodos(data);
+
+    })
+  },[]);
+
+ function newTaskCreated(e){
+  e.preventDefault();
+  fetch("http://localhost:3000/posttodo",{
+    method:"POST",
+    headers:{
+      "Content-type":"Application/json"
+    },
+    body:JSON.stringify({name:todoName,description:todoDes})    
+  })
+  .then((response)=>{
+     return response.json();
+  })
+  .then((data)=>{
+      console.log(data);
+      setTodos(data);
+      setTodoName("");
+      setTodoDes("");
+
+  })
+
+  }
   return (
     <div>
-      this is task
+      <form onSubmit={newTaskCreated}>
+    <input type="text"  placeholder="Enter a task name" value={todoName} onChange={(e)=>setTodoName(e.target.value)}/>
+    <input type="text" placeholder="Enter a task description" value={todoDes} onChange={(e)=>setTodoDes(e.target.value)}/>
+    <button type="submit" >create</button>
+</form>
+      <div>{todos.map((item)=>{
+        return <div key={item.id}>
+          <li>{item.name}</li>
+          <li>{item.description}</li>
+            </div> })}</div>
+
     </div>
   )
 }
